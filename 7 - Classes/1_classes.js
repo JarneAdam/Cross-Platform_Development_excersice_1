fea804b82ebabcacb6083941f360c665a447ef28
Jarne Adam <jarne.adam@student.vives.be>
2025-10-01 23:41:08 +0200

    Finished excersise 7.1

--- 7 - Classes/1_classes.js
+++ 7 - Classes/1_classes.js
@@ -1,9 +1,12 @@
 /**
  * Gebruik een ES2015 class om een Person object aan te maken.
  */
-function Person(firstname, lastname) {
-  this.fname = firstname;
-  this.lname = lastname;
+
+class Person {
+  constructor(firstname, lastname) {
+    this.fname = firstname;
+    this.lname = lastname;
+  }
 }
 
 const john = new Person("John", "Doe");
