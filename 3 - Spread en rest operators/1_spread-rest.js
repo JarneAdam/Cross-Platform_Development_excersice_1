a65862fc1a8d36698a1b0d3074309d941b72569b
Jarne Adam <jarne.adam@student.vives.be>
2025-10-01 12:37:06 +0200

    Finished excersice 3

--- 3 - Spread en rest operators/1_spread-rest.js
+++ 3 - Spread en rest operators/1_spread-rest.js
@@ -2,16 +2,7 @@
  * Herschrijf sumArgs() in ES2015, gebruikmakend van een rest parameter, block scoped variables en arrow functions.
  * Het moet mogelijk zijn om het volgende uit te voeren: sumArgs(1, 2, 3, 4, 5, 6);
  */
-var arguments = [1, 2, 3, 4, 5, 6]; // verwijder deze lijn
 
-function sumArgs(arguments) {
-  var result = 0;
+const sumArgs = (...args) => args.reduce((result, number) => result + number, 0)
 
-  for (var i = 0; i < arguments.length; ++i) {
-    result += arguments[i];
-  }
-
-  return result;
-}
-
-console.log(sumArgs(arguments));
+console.log(sumArgs(1, 2, 3, 4, 5, 6));
