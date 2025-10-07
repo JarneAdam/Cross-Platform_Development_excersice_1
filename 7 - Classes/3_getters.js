2f4ea889b4e297f0ede9feb273df1d11c4e7d7a2
Jarne Adam <jarne.adam@student.vives.be>
2025-10-07 21:46:24 +0200

    Finished exercise 7.3

--- 7 - Classes/3_getters.js
+++ 7 - Classes/3_getters.js
@@ -5,8 +5,10 @@ class Person {
   constructor(firstname, lastname) {
     this.fname = firstname;
     this.lname = lastname;
+  }
 
-    this.fullName = this.fname + ' ' + this.lname;
+  get fullName() {
+    return this.fname + ' ' + this.lname;
   }
 }
 
